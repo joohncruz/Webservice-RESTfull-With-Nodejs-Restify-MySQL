991be7d71628f6dfbbb17423f7b75e996bc8db55
Jonathan Cruz Rocha Ferreira <joohncruzrocha@gmail.com>
2017-09-13 18:03:59 -0300

    Trancando a tabela de categoria depois do teste

--- src/services/mysql/tests/categories.test.js
+++ src/services/mysql/tests/categories.test.js
@@ -1,11 +1,10 @@
 
 const test = require('ava')
-
 const { connection, errorHandler } = require('./setup')
 
 const categories = require('../categories')({ connection, errorHandler })
 
-test.beforeEach(t => )
+test.beforeEach(t => connection.query('TRUNCATE table categories'))
 
 test('Criação de Categoria', async t => {
   const result = await categories.save('category-test')
