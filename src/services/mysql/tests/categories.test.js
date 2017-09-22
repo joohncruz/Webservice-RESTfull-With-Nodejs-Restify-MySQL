76f7fedbad19d7d079b56d0bf666bb39c0e6c52d
Jonathan Ferreira <joohncruzrocha@gmail.com>
2017-09-21 23:45:33 -0300

    Criando teste para busca de todas as categorias

--- src/services/mysql/tests/categories.test.js
+++ src/services/mysql/tests/categories.test.js
@@ -1,12 +1,18 @@
 
 const test = require('ava')
 const { connection, errorHandler } = require('./setup')
-
 const categories = require('../categories')({ connection, errorHandler })
 const create = () => categories.save('category-test')
 
 test.beforeEach(t => connection.query('TRUNCATE table categories'))
 test.after.always(t => connection.query('TRUNCATE table categories'))
+
+test('Lista de Categoria', async t => {
+  await create()
+  const list = await categories.all()
+  t.is(list.categories.length, 1)
+  t.is(list.categories[0].name, 'category-test')
+})
 
 test('Criação de Categoria', async t => {
   const result = await create()
