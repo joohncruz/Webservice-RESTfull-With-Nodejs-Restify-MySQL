571d6d927a4402ca3fd1bbc980793205e7c1575c
Jonathan Ferreira <joohncruzrocha@gmail.com>
2017-09-24 12:43:36 -0300

    Criando atutenticacao via jwt

--- src/http/routes.js
+++ src/http/routes.js
@@ -2,47 +2,53 @@
 const db = require('../services/mysql')
 
 const routes = (server) => {
+  server.post('autenticacao', async (req, res, next) => {
+    try {
+      const {email, password} = req.params
+      res.send(await db.auth().authenticate(email, password))
+    } catch (error) {
+      res.send(error)
+    }
+    next()
+  })
+
   server.get('categoria', async (req, res, next) => {
     try {
       res.send(await db.categories().all())
-      next()
     } catch (error) {
       res.send(error)
-      next()
     }
+    next()
   })
 
   server.post('categoria', async (req, res, next) => {
     const {name} = req.params
     try {
       res.send(await db.categories().save(name))
-      next()
     } catch (error) {
       res.send(error)
-      next()
     }
+    next()
   })
 
   server.put('categoria', async (req, res, next) => {
     const { id, name } = req.params
     try {
       res.send(await db.categories().update(id, name))
-      next()
     } catch (error) {
       res.send(error)
-      next()
     }
+    next()
   })
 
   server.del('categoria', async (req, res, next) => {
     const { id } = req.params
     try {
       res.send(await db.categories().del(id))
-      next()
     } catch (error) {
       res.send(error)
-      next()
     }
+    next()
   })
 
   server.get('/', (req, res, next) => {
